93b64576d29705655abb14a5a9ba6a24876e9971
DenisMah <deenkom37@gmail.com>
2024-05-04 07:39:59 +0200

    Minor changes

--- script.js
+++ script.js
@@ -5,15 +5,25 @@ let grandma = 0;
 const upgrade = document.querySelector("#bigupgrade");
 const granny = document.querySelector("#granny");
 
+const grannydesc = document.createElement('div');
+
+
+granny,addEventListener("mouseover" , e =>{
+    grannydesc.classList.add('pop');
+    grannydesc.innerText = "moveyourbody";
+    granny.appendChild(grannydesc)
+})
+granny.addEventListener("mouseleave" , e => {
+
+    grannydesc.style.display = "none";
+    console.log("##33")
+    document.removeChild(grannydesc)
+    });
 cookie.addEventListener("click", e =>{
     console.log("clicked")
-
     money++;
-
     moneyElement.innerText = `Money earned: ${money}$`
-
     cookie.classList.add('enlarge'); // Add the spin class
-
     setTimeout(() => {
         cookie.classList.remove('enlarge');  // Remove the class after the animation
     }, 300); // Adjust the delay if needed
@@ -41,10 +51,20 @@ function tableUpdate (){
 
 
 function grannyMoney() {
-    money+= grandma * 1z;
+    money+= grandma * 1;
     moneyElement.innerText = `Money earned: ${money}$`;
 }
 
+
+upgrade.addEventListener("click", e =>{
+    if(money > 3){
+        setInterval(upradeCookie, 5000);
+    }
+})
+
+function upradeCookie(){
+    money += money * 0.10;
+}
 
 
 setInterval(tableUpdate, 1);
