1b0eaf13a61405c72b4b1a2f070dcfbdb29d470e
DenisMah <deenkom37@gmail.com>
2024-05-05 19:27:41 +0200

    MINOR CHANGES

--- script.js
+++ script.js
@@ -1,71 +1,59 @@
 const cookie = document.querySelector("#cookie");
 const moneyElement = document.querySelector("#money");
 let money = 0;
-let grandma = 0;
-const upgrade = document.querySelector("#bigupgrade");
-const granny = document.querySelector("#granny");
-
-const grannydesc = document.createElement('div');
 
 
-granny,addEventListener("mouseover" , e =>{
-    grannydesc.classList.add('pop');
-    grannydesc.innerText = "moveyourbody";
-    granny.appendChild(grannydesc)
+// Handle the modal and 
+const bakeryname = document.querySelector("#bakeryname");
+const modal = document.querySelector(".bakerymodal");
+const closemodal = document.querySelector("#closemodal");
+
+
+function updateMoneyElement(){
+    moneyElement.innerHTML = `${money}$`;
+}
+setInterval(updateMoneyElement, 50);
+
+bakeryname.addEventListener('click', () => {
+    showModal();
+    console.log("cookie")
 })
-granny.addEventListener("mouseleave" , e => {
 
-    grannydesc.style.display = "none";
-    console.log("##33")
-    document.removeChild(grannydesc)
-    });
+
+bakeryname.addEventListener('click', () => {
+showModal();
+console.log("cookie")
+})
+
+modal.addEventListener('input', e =>{
+    bakeryname.innerHTML = e.target.value;
+})
+
+closemodal.addEventListener('click', () => {
+    closeModal();
+})
+
+
 cookie.addEventListener("click", e =>{
     console.log("clicked")
     money++;
-    moneyElement.innerText = `Money earned: ${money}$`
+    moneyElement.innerText = `${money}$`
     cookie.classList.add('enlarge'); // Add the spin class
     setTimeout(() => {
         cookie.classList.remove('enlarge');  // Remove the class after the animation
     }, 300); // Adjust the delay if needed
 })
 
+function closeModal(){
+    modal.style.display = "none";
+}
 
-
-granny.addEventListener("click", e =>{
-    if(money >= 50){
-        money = money-50;
-        grandma++;
-        moneyElement.innerText = `Money earned: ${money}$`
-    }
-})
-
-
-
-function tableUpdate (){
-    if (money > 50){
-        granny.classList.add('green')
-    } else {
-        granny.classList.remove('green')
-    }
+function showModal(){
+    modal.style.display = "flex";
 }
 
 
-function grannyMoney() {
-    money+= grandma * 1;
-    moneyElement.innerText = `Money earned: ${money}$`;
-}
 
 
-upgrade.addEventListener("click", e =>{
-    if(money > 3){
-        setInterval(upradeCookie, 5000);
-    }
-})
-
-function upradeCookie(){
-    money += money * 0.10;
-}
 
 
-setInterval(tableUpdate, 1);
-setInterval(grannyMoney, 3000);
