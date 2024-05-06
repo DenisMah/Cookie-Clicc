9d17a7e46a11fe4c997326499c16486d8827db9f
DenisMah <deenkom37@gmail.com>
2024-05-06 17:40:53 +0200

    Minor changes

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // <-----------------------------------------------------------> //
 // Handle money and updating the money dynamically//
 let money = 0;
-
+const moneyElement = document.querySelector('#money');
 function updateMoney(){
     let totalMoney = 1*grandmacount;
     moneyPerSecond.innerHTML = `Per second: ${totalMoney}`;
@@ -12,7 +12,8 @@ setInterval(updateMoney,1000);
 
 const moneyPerSecond = document.querySelector('.persec');
 
-
+moneyPerSecond.innerHTML = `
+`
 // <-----------------------------------------------------------> //
 // Handle the modal and allow bakery name change //
 const openmodal = document.querySelector("#bakeryname");
@@ -50,13 +51,41 @@ cookie.addEventListener("click", e =>{
     }, 300);
 })
 // <-----------------------------------------------------------> //
+const cursorinfo = document.querySelector("#cursorinfo");
+let clickerCount = 0;
+const clickerBasePrice = 10;
+let dynamicClickerPrice = clickerBasePrice + (clickerBasePrice * clickerCount);
+// Purchase clicker and handle clicker upgrades
+const clicker = document.querySelector('#clicker');
+clicker.addEventListener('click', e =>{
+    if (money > dynamicClickerPrice){
+        clickerCount++;
+        money = money - dynamicClickerPrice;
+    }
+})
 
+
+function clickerMoney(){
+    money+=0.25*clickerCount;
+}
+setInterval(clickerMoney,1000);
+
+function updateClickerPrice(){
+    dynamicClickerPrice = clickerBasePrice + (clickerBasePrice * clickerCount) * 0.25;
+    cursorinfo.innerHTML = `
+    <p> Clicker -</p>
+    <p class="price">${dynamicClickerPrice}$</p>
+  `
+}
+setInterval(updateClickerPrice,1000);
+
+// <-----------------------------------------------------------> //
 // Purchase grandma and handle grandma upgrades //
 let grandmacount = 0;
 const granny = document.querySelector("#granny");
 const grandmaBasePrice = 50;
 let dynamicGrandmaPrice = grandmaBasePrice + (grandmaBasePrice * grandmacount);
-let grandmaprice = document.querySelector("#grandmaprice");
+let grandmaprice = document.querySelector("#grandmainfo");
 
 
 granny.addEventListener("click", e =>{
@@ -72,7 +101,9 @@ setInterval(grandmaMoney, 1000);
 
 function updateGrandmaPrice(){
     dynamicGrandmaPrice = grandmaBasePrice + (grandmaBasePrice * grandmacount) * 0.25;
-    grandmaprice.innerText = `Grandma - ${dynamicGrandmaPrice}$`;
+    grandmaprice.innerHTML = `
+    <p> Grandma -</p> 
+    <p class = "price">${dynamicGrandmaPrice}$ </p>`;
 }
 setInterval(updateGrandmaPrice,1000);
 // <-----------------------------------------------------------> //
