46e59f4e557ace539f00bf390eaa9eef5def840a
DenisMah <deenkom37@gmail.com>
2024-05-16 09:19:28 +0200

    Minor changes ( changed the entire project xD )

--- Fork_2/script.js
+++ Fork_2/script.js
@@ -2,8 +2,9 @@
 // ? Here I put all my query selectors in one place.
 const purchaseMenu = document.querySelector('.purchase-menu');
 const shoppingcart = document.querySelector('#shopping-cart');
-const button = document.querySelector('#close-button');
-
+const button = document.querySelectorAll('.close-purchase-menu-span');
+const navbar = document.querySelector('.navbar')
+const menu = document.querySelectorAll('.menu-span')
 
 
 //* This allows me to toggle on and off my purchase-menu div & adds an animation to the popup.
@@ -21,12 +22,42 @@ shoppingcart.addEventListener('click', ev =>{
 
 //* This adds a click listener to the button which causes is to collapse my purchase menu div.
 
-button.addEventListener('click', ev =>{
-    purchaseMenu.classList.add('animation');
-    setTimeout(()=>{
-        purchaseMenu.style.display= 'none';
-        purchaseMenu.classList.remove('animation');
-    },499);
+button.forEach((buttons) =>{
+    buttons.addEventListener('click', ev =>{
+        purchaseMenu.classList.add('animation');
+        setTimeout(()=>{
+            purchaseMenu.style.display= 'none';
+            purchaseMenu.classList.remove('animation');
+        },499);
+    })
+})
+
+//* Create a popup below the hamburger menu.
+
+menu.forEach((menuspan) =>{
+    menuspan.addEventListener('click', ev =>{
+        const menunav = document.createElement('div');
+        menunav.classList.add('menu-nav')
+        menunav.innerHTML = `
+        <span class='menuspan-close'></span><span class='menuspan-close'></span>
+        `
+        navbar.appendChild(menunav)
+        menunav.classList.add('animation-slidein')
+
+
+        //* This closes the menu
+        const menuspanclose = document.querySelectorAll('.menuspan-close')
+
+        menuspanclose.forEach((menuitem) =>{
+            menuitem.addEventListener('click', ev =>{
+                menunav.classList.add('animation-slideup')
+                setTimeout(()=>{
+                    navbar.removeChild(menunav)
+                }, 499)
+
+            })
+        })
+    })
 })
 
 
