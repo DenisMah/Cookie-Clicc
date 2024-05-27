272daefdcb432bf99abba419250660b63eb57116
DenisMah <deenkom37@gmail.com>
2024-05-27 21:01:56 +0200

    Adding form validations & loading people

--- JavaScript/get-involved.js
+++ JavaScript/get-involved.js
@@ -1,35 +1,15 @@
-// Wait for the DOM to be ready
-$(function() {
-    // Initialize form validation on the registration form.
-    // It has the name attribute "registration"
-    $("form[name='registration']").validate({
-        // Specify validation rules
-        rules: {
-            firstname: "required",
-            lastname: "required",
-            email: {
-                required: true,
-                email: true
-            },
-            password: {
-                required: true,
-                minlength: 8
-            }
-        },
-        // Specify validation error messages
-        messages: {
-            firstname: "Please enter your first name",
-            lastname: "Please enter your last name",
-            email: "Please enter a valid email address",
-            password: {
-                required: "Please provide a password",
-                minlength: "Your password must be at least 8 characters long"
-            }
-        },
-        // Make sure the form is submitted to the destination defined
-        // in the "action" attribute of the form when valid
-        submitHandler: function(form) {
-            form.submit();
-        }
-    });
-});
+$(document).ready(function(){
+    $.getJSON('JSON/people.json', function(data){
+        data.forEach(element => {
+            const person = document.createElement('div');
+            $(person).addClass('person');
+            person.innerHTML = `
+            <p id = "element-name">${element.name}</p>
+            <img src = ${element.image}>
+            <p> " ${element.description}"</p>
+            `
+
+            $('#our-people').append(person)
+        });
+    })
+})
