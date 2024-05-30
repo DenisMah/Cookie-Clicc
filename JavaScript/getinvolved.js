13eaf7f70eb31342f0e1c6177115f27f87f93153
DenisMah <deenkom37@gmail.com>
2024-05-30 18:01:14 +0200

    Form is finished

--- JavaScript/getinvolved.js
+++ JavaScript/getinvolved.js
@@ -1,47 +1,48 @@
-
-$('#myForm').validate({
+$(document).ready(function () {
+  $("#myForm").validate({
     rules: {
-        name: 'required',
-        email: {
-            required: true,
-            email: true
-        },
-        password: {
-            required: true,
-            minlength: 8,
-            maxlength: 20
-        }
+      name: "required",
+      email: {
+        required: true,
+        email: true,
+      },
+      age: {
+        required: true,
+        number: true,
+        min: 18,
+        max: 100,
+      },
     },
     messages: {
-        name: 'Please enter your name',
-        email: {
-            required: 'Please enter your email',
-            email: 'Please enter a valid email address'
-        },
-        password: {
-            required: 'Please enter a password',
-            minlength: 'Password must be at least 8 characters long',
-            maxlength: 'Password cannot be longer than 20 characters',
-        }
+      name: "Please enter your name",
+      email: {
+        required: "Please enter your email",
+        email: "Please enter a valid email address",
+      },
     },
     submitHandler: function (form) {
-        try {
-            // Block UI on form submission
-            $.blockUI({ message: '<div class="spinner-border" role="status"><span class="visually-hidden">Loading...</span></div>' });
-            // Reset the form
-            $(form)[0].reset();
-            // Unblock UI after request is complete
+      try {
+        //* Block UI on form submission
 
-            setTimeout(function(){
-                console.log('time out')
-                $.unblockUI();
-            }, 3000)
-            
-        } catch (error) {
-            console.error('Error in submitHandler:', error);
-            alert('An error occurred. Please try again later.');
-            // Unblock UI after request is complete
-            $.unblockUI();
-        }
-    }
+        $.blockUI({
+          message:
+            '<div class="spinner-border" role="status"><span class="visually-hidden">Loading...</span></div>',
+        });
+
+        //* Simulate an AJAX request
+        setTimeout(function () {
+          $(form)[0].reset();
+
+          $.unblockUI();
+
+          toastr.success("Data saved successfully!");
+        }, 3000);
+      } catch (error) {
+        console.error("Error in submitHandler:", error);
+        alert("An error occurred. Please try again later.");
+
+        $.unblockUI();
+      }
+    },
+  });
 });
